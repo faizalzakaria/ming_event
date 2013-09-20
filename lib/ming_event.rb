f8e50063db2fd8ba965a5883f6ffb6e87d2af9c9
Faizal Zakaria <phaibusiness@gmail.com>
2013-09-20 10:12:32 -0700

    Untabify

--- lib/ming_event.rb
+++ lib/ming_event.rb
@@ -18,103 +18,103 @@ module MingEvent
 
   class Timer
 
-	def initialize(second, &block)
-	  @thread = Thread.new do
-		sleep(second)
-		block.call
-	  end
-	end
+    def initialize(second, &block)
+      @thread = Thread.new do
+        sleep(second)
+        block.call
+      end
+    end
 
-	def wait
-	  @thread.join
-	end
+    def wait
+      @thread.join
+    end
 
-	def cancel
-	  @thread.kill
-	end
+    def cancel
+      @thread.kill
+    end
 
   end
 
   class Gender
-	class << self
-	  def male
-		"MALE"
-	  end
-	  def female
-		"FEMALE"
-	  end
-	end
+    class << self
+      def male
+        "MALE"
+      end
+      def female
+        "FEMALE"
+      end
+    end
   end
 
   class User
 
-	# Connection, conencted to ith when its true
-	attr_reader :gender, :user_id, :connection
+    # Connection, conencted to ith when its true
+    attr_reader :gender, :user_id, :connection
 
-	def initialize(user_id, gender)
-	  @user_id = user_id
-	  @connection = Hash.new
-	  # Todo check gender class
-	  @gender = gender
-	end
+    def initialize(user_id, gender)
+      @user_id = user_id
+      @connection = Hash.new
+      # Todo check gender class
+      @gender = gender
+    end
 
-	def is_same_gender?(to_user)
-	  return @gender == to_user.gender
-	end
+    def is_same_gender?(to_user)
+      return @gender == to_user.gender
+    end
 
-	def is_connected?(to_user)
-	  return true if is_same_gender?(to_user)
-	  return @connection[:connected] || false
-	end
+    def is_connected?(to_user)
+      return true if is_same_gender?(to_user)
+      return @connection[:connected] || false
+    end
 
-	def connect_to(user)
-	  @connection[:connected] = true
-	end
+    def connect_to(user)
+      @connection[:connected] = true
+    end
 
   end
 
   class Event
 
-	def initialize
-	end
+    def initialize
+    end
 
-	# Register all the observers def register(observers, interval)
-	def register(female_users, male_users)
-	  if !female_users.nil?
-		female_users.each do |user_id|
-		  register_user user_id Gender.female
-		end
-	  end
+    # Register all the observers def register(observers, interval)
+    def register(female_users, male_users)
+      if !female_users.nil?
+        female_users.each do |user_id|
+          register_user user_id Gender.female
+        end
+      end
 
-	  if !male_users.nil?
-		male_users.each do |user_id|
-		  register_user user_id Gender.male
-		end
-	  end
-	end
+      if !male_users.nil?
+        male_users.each do |user_id|
+          register_user user_id Gender.male
+        end
+      end
+    end
 
-	def register_user(user_id, gender)
-	  if gender == Gender.male
-		@male_users << User.new(user_id, gender)
-	  elsif gender == Gender.female
-		@female_users << User.new(user_id, gender)
-	  else
-		false
-	  end
-	end
+    def register_user(user_id, gender)
+      if gender == Gender.male
+        @male_users << User.new(user_id, gender)
+      elsif gender == Gender.female
+        @female_users << User.new(user_id, gender)
+      else
+        false
+      end
+    end
 
-	# TODO
-	def start
-	  Timer.new(interval, &method(:fire_observers))
-	end
+    # TODO
+    def start
+      Timer.new(interval, &method(:fire_observers))
+    end
 
-	# TODO
-	def get_next_pair(user)
+    # TODO
+    def get_next_pair(user)
 
-	end
+    end
 
-	def fire_observers
-	  puts "Done ..."
-	end
+    def fire_observers
+      puts "Done ..."
+    end
   end
 end
--- spec/ming_event_spec.rb
+++ spec/ming_event_spec.rb
@@ -4,23 +4,23 @@ describe MingEvent do
 
   it "Timer 5 seconds " do
 
-	fired = false
-	t = MingEvent::Timer.new(1) do
-	  fired = true
-	end
+    fired = false
+    t = MingEvent::Timer.new(1) do
+      fired = true
+    end
 
-	t.wait
-	fired.should be_true
+    t.wait
+    fired.should be_true
   end
 
   it "Should add user " do
-	user = MingEvent::User.new(1, MingEvent::Gender.male)
-	user.user_id.should be 1
+    user = MingEvent::User.new(1, MingEvent::Gender.male)
+    user.user_id.should be 1
   end
 
   it "Should not add user " do
-	user = MingEvent::User.new(1, "tata")
-	#user.should be nil
+    user = MingEvent::User.new(1, "tata")
+    #user.should be nil
   end
 
 end
