81e7878fc9fad9e2f2672c7e933eedff5dc1875e
Faizal Zakaria <phaibusiness@gmail.com>
2013-09-22 18:37:56 -0700

    Add event Algorithm, using linked list

--- lib/ming_event.rb
+++ lib/ming_event.rb
@@ -16,8 +16,8 @@ require "ming_event/version"
 
 module MingEvent
 
+  # Timer class
   class Timer
-
     def initialize(second, &block)
       @thread = Thread.new do
         sleep(second)
@@ -32,7 +32,6 @@ module MingEvent
     def cancel
       @thread.kill
     end
-
   end
 
   class Gender
@@ -40,81 +39,175 @@ module MingEvent
       def male
         "MALE"
       end
+
       def female
         "FEMALE"
       end
     end
-  end
+  end # End class Gender
 
   class User
-
-    # Connection, conencted to ith when its true
-    attr_reader :gender, :user_id, :connection
+    # Connections, conencted to ith when its true
+    attr_reader :gender, :user_id
+    attr_accessor :next_partner
 
     def initialize(user_id, gender)
       @user_id = user_id
-      @connection = Hash.new
+      @connections = Hash.new
       # Todo check gender class
       @gender = gender
+      @next_partner = nil
     end
 
     def is_same_gender?(to_user)
       return @gender == to_user.gender
     end
+  end # End class User
 
-    def is_connected?(to_user)
-      return true if is_same_gender?(to_user)
-      return @connection[:connected] || false
+  module LinkedList
+    class Node
+      attr_accessor :next, :obj
+
+      def initialize(obj)
+        @next = nil
+        @obj = obj
+      end
     end
 
-    def connect_to(user)
-      @connection[:connected] = true
-    end
+    class List
+      attr_reader :head, :size
 
+      def initialize
+        @head = nil
+        @size = 0
+      end
+
+      def add(obj)
+        new_node = Node.new(obj)
+        if @head.nil?
+          @head = new_node
+        else
+          temp_node = @head
+          while !temp_node.next.nil?
+            temp_node = temp_node.next
+          end
+          temp_node.next = new_node
+        end
+        @size += 1
+      end
+
+      def remove(node)
+        if node.eql? @head
+          @head = node.next
+        else
+          temp_node = @head
+          prev = nil
+          while !temp_node.nil?
+            if temp_node.eql?(node)
+              prev.next = node.next
+              break
+            end
+            prev = temp_node
+            temp_node = temp_node.next
+          end
+        end
+        node.next = nil
+        @size -= 1
+        GC.start
+      end
+    end
   end
 
   class Event
+    attr_reader :male_list, :female_list
+    attr_accessor :delay_chat, :delay_idle
 
-    def initialize
+    def initialize(delay_chat = 300, delay_idle = 30)
+      @delay_chat = delay_chat
+      @delay_idle = delay_idle
+      @male_list = LinkedList::List.new
+      @female_list = LinkedList::List.new
     end
 
     # Register all the observers def register(observers, interval)
-    def register(female_users, male_users)
+    # TODO : rename
+    def register_users(female_users, male_users)
       if !female_users.nil?
         female_users.each do |user_id|
-          register_user user_id Gender.female
+          register_user user_id, Gender.female
         end
       end
 
       if !male_users.nil?
         male_users.each do |user_id|
-          register_user user_id Gender.male
+          register_user user_id, Gender.male
         end
       end
     end
 
     def register_user(user_id, gender)
       if gender == Gender.male
-        @male_users << User.new(user_id, gender)
+        @male_list.add User.new(user_id, gender)
       elsif gender == Gender.female
-        @female_users << User.new(user_id, gender)
+        @female_list.add User.new(user_id, gender)
       else
         false
       end
     end
 
-    # TODO
-    def start
-      Timer.new(interval, &method(:fire_observers))
+    def prepare
+      male = @male_list.head
+      female = @female_list.head
+      while !male.nil?
+        male.obj.next_partner = female
+        female.obj.next_partner = male
+        male = male.next
+        female = female.next
+      end
     end
 
-    # TODO
-    def get_next_pair(user)
+    def start(procs)
+      t = Timer.new(@delay_idle, &procs[:connect_cb])
+      t.wait
 
+      (2..@male_list.size).each do
+        t = Timer.new(@delay_chat, &procs[:idle_cb])
+        t.wait
+        t = Timer.new(@delay_idle, &procs[:connect_cb])
+        t.wait
+      end
+
+      t = Timer.new(@delay_idle, &procs[:idle_cb])
+      t.wait
     end
 
-    def fire_observers
-      puts "Done ..."
+    def next_partners
+      male = @male_list.head
+      while !male.nil?
+        next_partner male
+        male = male.next
+      end
     end
-  end
+
+    def next_partner(male)
+      if (male.obj.gender == Gender.female)
+        # Do nothing, only move male
+        p "Should only handle Male"
+      elsif (male.obj.gender == Gender.male)
+        next_female = male.obj.next_partner.next || @female_list.head
+        male.obj.next_partner = next_female
+        next_female.obj.next_partner = male
+      else
+        p "Raise error!"
+      end
+    end
+
+    def command_to_connect
+      puts "Command to connect"
+    end
+
+    def command_to_idle
+      puts "Command to idle"
+    end
+  end # End class Event
 end
--- spec/ming_event_spec.rb
+++ spec/ming_event_spec.rb
@@ -2,25 +2,137 @@ require 'spec_helper'
 
 describe MingEvent do
 
-  it "Timer 5 seconds " do
+  describe "Testing Timer" do
+    it "Timer 5 seconds " do
+      fired = false
+      t = MingEvent::Timer.new(1) do
+        fired = true
+      end
 
-    fired = false
-    t = MingEvent::Timer.new(1) do
-      fired = true
+      t.wait
+      fired.should be_true
+    end
+  end
+
+  describe "Testing User" do
+    it "Should add user " do
+      user = MingEvent::User.new(1, MingEvent::Gender.male)
+      user.user_id.should be 1
     end
 
-    t.wait
-    fired.should be_true
+    # TODO, testing the raise error on gender
+    it "Should not add user " do
+      user = MingEvent::User.new(1, "tata")
+      #user.should be nil
+    end
   end
 
-  it "Should add user " do
-    user = MingEvent::User.new(1, MingEvent::Gender.male)
-    user.user_id.should be 1
+  describe "Testing linkedList ..." do
+    it "@head should return 'A'" do
+      list = MingEvent::LinkedList::List.new
+      list.add("A")
+      list.head.obj.should eq("A")
+    end
+
+    it "@head.next should return 'B'" do
+      list = MingEvent::LinkedList::List.new
+      list.add("A")
+      list.add("B")
+      list.head.next.obj.should eq("B")
+    end
+
+    it "@head.next should return 'C'" do
+      list = MingEvent::LinkedList::List.new
+      list.add("A")
+      list.add("B")
+      list.add("C")
+      list.remove(list.head.next)
+      list.head.next.obj.should eq("C")
+    end
+
+    it "size should return 3" do
+      list = MingEvent::LinkedList::List.new
+      list.add("A")
+      list.add("B")
+      list.add("C")
+      list.size.should eq(3)
+    end
   end
 
-  it "Should not add user " do
-    user = MingEvent::User.new(1, "tata")
-    #user.should be nil
-  end
+  describe "Testing Event" do
+    it "should have 5 males and 5 females" do
+      event = MingEvent::Event.new
+      female_users = [1,2,3,4,5]
+      male_users = [6,7,8,9,10]
+      event.register_users(female_users, male_users)
+      size_male = 0
+      size_female = 0
+      t = event.male_list.head
+      while !t.nil?
+        size_male += 1
+        t = t.next
+      end
+      t = event.female_list.head
+      while !t.nil?
+        size_female += 1
+        t = t.next
+      end
+      size_male.should eq(5)
+      size_female.should eq(5)
+    end
 
+    it "should have correct next partner" do
+      event = MingEvent::Event.new
+      female_users = [1,2,3,4,5]
+      male_users = [6,7,8,9,10]
+      event.register_users(female_users, male_users)
+      event.prepare
+      event.male_list.head.obj.next_partner.should eq(event.female_list.head)
+    end
+
+    it "should have correct next partner after next_partner has been called" do
+      event = MingEvent::Event.new
+      female_users = [1,2,3,4,5]
+      male_users = [6,7,8,9,10]
+      event.register_users(female_users, male_users)
+      event.prepare
+      event.next_partner(event.male_list.head)
+      event.next_partner(event.male_list.head)
+      event.male_list.head.obj.next_partner.should eq(event.female_list.head.next.next)
+    end
+
+    it "should have correct next partner after next_partners has been called" do
+      event = MingEvent::Event.new
+      female_users = [1,2,3,4,5]
+      male_users = [6,7,8,9,10]
+      event.register_users(female_users, male_users)
+      event.prepare
+      event.next_partners
+      event.male_list.head.obj.next_partner.should eq(event.female_list.head.next)
+      event.male_list.head.next.obj.next_partner.should eq(event.female_list.head.next.next)
+      last = event.male_list.head.next.next.next.next
+      last.obj.next_partner.should eq(event.female_list.head)
+    end
+
+    it "Should get 5 connect call and 5 idle call" do
+      event = MingEvent::Event.new(1, 1)
+      female_users = [1,2,3,4,5]
+      male_users = [6,7,8,9,10]
+      event.register_users(female_users, male_users)
+      event.prepare
+      connect_call = 0
+      idle_call = 0
+      t = Thread.new do
+        callbacks = {
+          :connect_cb => Proc.new{ connect_call += 1 },
+          :idle_cb => Proc.new{ idle_call += 1 }
+        }
+        event.start(callbacks)
+      end
+      t.join
+      connect_call.should eq(5)
+      idle_call.should eq(5)
+    end
+
+  end
 end
